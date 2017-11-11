3dbca39118f2b6c671685ca7c7b603dcc0d2a2b4
Amir Sher <locosher@gmail.com>
2017-11-11 13:06:11 +0200

    # upload from local #

--- podfox.py
+++ podfox.py
@@ -222,13 +222,13 @@ def download_single(folder, url, title):
         base = CONFIGURATION['podcast-directory']
     filename = url.split('/')[-1]
     filename = filename.split('?')[0]
-    print_red("....." + "{:s} .....".format(filename))
+#    print_red("....." + "{:s} .....".format(filename))
+    print_red(title)
     r = requests.get(url.strip(), stream=True)
     with open(os.path.join(base, folder, filename), 'wb') as f:
         for chunk in r.iter_content(chunk_size=1024**2):
             f.write(chunk)
-    print(title)
-    print('\n' + "done." + '\n')
+    print("done." + '\n')
 
 
 def available_feeds():
